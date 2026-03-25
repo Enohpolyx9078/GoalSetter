7dff9b523f8ddd62e3b3220067056363f2dcd3e4
Enohpolyx9078 <hicrawford982@gmail.com>
2026-03-24 19:27:08 -0600

    Stubbed out start component

--- src/app.jsx
+++ src/app.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './neubrutalism.css';
 import './app.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Start } from './flow/start.jsx'
 
 export default function App() {
     return (
         <BrowserRouter>
             <div className="p-4">
                 <Routes>
+                    <Route path='/' element={<Start />} exact />
                     <Route path='*' element={<NotFound />} />
                 </Routes>
-                <footer className="text-body nb-card">
+                <footer className="absolute inset-x-4 bottom-4 text-body nb-card">
                     <p>Authored by Howard Crawford
                         <a href="https://github.com/Enohpolyx9078/GoalSetter" className="nb-btn ms-2" target="_blank"> Github</a>
                     </p>
