37936c76552df54407169743e6963105e0552034
Enohpolyx9078 <hicrawford982@gmail.com>
2026-03-24 19:37:04 -0600

    Mocked out start page

--- src/flow/start.jsx
+++ src/flow/start.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export function Start () {
-    return <div></div>
+export function Start() {
+    return (
+        <main>
+            <h1 className="text-display text-3xl nb-card">Ready to get started?</h1>
+            <section className="mt-4">
+                <h2 className="text-heading text-2xl">Name your goal:</h2>
+                <input className="nb-input" name="goalName" placeholder="Type your future..." />
+                <button className="nb-btn mt-4">Keep Going</button>
+            </section>
+        </main>
+    );
 }
