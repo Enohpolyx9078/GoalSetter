b442a1c26b0e4c265bd884769db1931ddb31e6f6
Enohpolyx9078 <hicrawford982@gmail.com>
2026-03-30 13:22:40 -0600

    Updated start styles

--- src/flow/start.jsx
+++ src/flow/start.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 export function Start() {
     return (
         <main>
-            <h1 className="text-display text-3xl nb-card">Ready to get started?</h1>
+            <h1 className="text-display text-3xl nb-card nb-pink">Ready to get started?</h1>
             <section className="mt-4">
-                <h2 className="text-heading text-2xl">Name your goal:</h2>
-                <input className="nb-input" name="goalName" placeholder="Type your future..." />
-                <button className="nb-btn mt-4">Keep Going</button>
+                <div className="nb-card nb-yellow">
+                    <h2 className="text-heading text-2xl">Name your goal:</h2>
+                    <input className="nb-input" name="goalName" placeholder="Type your future..." />
+                    <button className="nb-btn mt-4">Keep Going</button>
+                </div>
             </section>
         </main>
     );
