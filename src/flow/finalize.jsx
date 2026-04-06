5479f03614e92694b30a720c5c970d96be76cff5
Enohpolyx9078 <hicrawford982@gmail.com>
2026-04-06 12:47:00 -0600

    Flavor text

--- src/flow/finalize.jsx
+++ src/flow/finalize.jsx
@@ -10,7 +10,7 @@ export function Finalize({ goalName, timeline, breakdown }) {
 
     return (
         <section>
-            <div className="nb-card nb-yellow">
+            <div className="nb-card nb-pink">
                 <h1 className="text-display text-3xl">{goalName}</h1>
                 <h2 className="text-heading text-2xl">To be done by { timeline }</h2>
             </div>
@@ -21,13 +21,15 @@ export function Finalize({ goalName, timeline, breakdown }) {
             <section className="mt-4">
                 <div className="nb-card nb-blue">
                     <h2 className="text-heading text-2xl">Wrapping Up...</h2>
-                    <div className="nb-card">
+                    <div className="nb-card nb-bg">
                         <img src="christ.jpeg"/>
                     </div>
                     <div className="text-body mt-4">
-
+                        <p>Remember, there's exactly one person that can truly get you to your goal.</p>
+                        <p>That's <strong>Jesus Christ</strong>!</p>
+                        <p>While you're making your plans, include Him in each step. That's what will get you to the end.</p>
                     </div>
-                    <button onClick={toNext} className="nb-btn nb-orange mt-4">View Your Goal</button>
+                    <button onClick={toNext} className="nb-btn nb-yellow mt-4">View Your Goal</button>
                 </div>
             </section>
         </section>
