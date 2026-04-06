5bc2ff3a6b701a311c8f5e61b571fa2eb5d83b90
Enohpolyx9078 <hicrawford982@gmail.com>
2026-04-06 13:01:13 -0600

    Framed photo

--- src/flow/ending.jsx
+++ src/flow/ending.jsx
@@ -12,7 +12,9 @@ export function Ending({ goalName, timeline, breakdown }) {
         <section>
             <p className="text-center text-body m-1">Screenshot this page!</p>
             <div className="flex justify-center nb-card nb-blue">
-                <img className="h-[100px]" src="christ.jpeg" />
+                <div className="nb-card">
+                    <img className="h-[100px]" src="christ.jpeg" />
+                </div>
             </div>
             <div className="nb-card nb-orange mt-4 ">
                 <h1 className="text-display text-2xl">Imma get <b>{goalName}</b> done by <b>{timeline}</b></h1>
